20567f4683214a239b74f616bdfc6e30d002c963
Erika Vestin <erbj1201@student.miun.se>
2023-12-21 12:01:59 +0100

    tagit bort mappar, uppdaterat små saker

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+//import mongoose
+const mongoose = require("mongoose");
+// Mongoose schema for model
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  }
+});
+// Exporting the Mongoose model
+module.exports = mongoose.model("Drink", drinkSchema);
